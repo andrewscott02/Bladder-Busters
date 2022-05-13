a1ded2ac4359612786b70d8e926b6bf1fa0dc2a8
ANDREW SCOTT <as243879@falmouth.ac.uk>
2022-05-13 20:04:48 +0100

    Jank movement

---
+++ Assets/Characters/Buster/JankMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JankMovement : MonoBehaviour
+{
+    public Dictionary<KeyCode, int> inputDirections;
+
+    public BusterController busterController;
+
+    private void Start()
+    {
+        inputDirections = new Dictionary<KeyCode, int>();
+
+        inputDirections.Add(KeyCode.W, 0);
+        inputDirections.Add(KeyCode.A, 1);
+        inputDirections.Add(KeyCode.S, 2);
+        inputDirections.Add(KeyCode.D, 3);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            //Debug.Log("W pressed: move " + DetermineDirection(KeyCode.W));
+            busterController.Movement(DetermineDirection(KeyCode.W));
+        }
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            //Debug.Log("A pressed: move " + DetermineDirection(KeyCode.A));
+            busterController.Movement(DetermineDirection(KeyCode.A));
+        }
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //Debug.Log("S pressed: move " + DetermineDirection(KeyCode.S));
+            busterController.Movement(DetermineDirection(KeyCode.S));
+        }
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            //Debug.Log("D pressed: move " + DetermineDirection(KeyCode.D));
+            busterController.Movement(DetermineDirection(KeyCode.D));
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Debug.Log("randomise");
+            RandomiseInputs();
+        }
+    }
+
+    int DetermineDirection(KeyCode keyCode)
+    {
+        return inputDirections[keyCode];
+    }
+
+    void RandomiseInputs()
+    {
+        List<int> newInputDirections = new List<int>();
+
+        for (int i = 0; i < 4; i++)
+        {
+            newInputDirections.Add(Random.Range(0, 4));
+        }
+
+        inputDirections[KeyCode.W] = newInputDirections[0];
+        inputDirections[KeyCode.A] = newInputDirections[1];
+        inputDirections[KeyCode.S] = newInputDirections[2];
+        inputDirections[KeyCode.D] = newInputDirections[3];
+    }
+}
